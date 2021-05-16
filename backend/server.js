2b65a3046d16926dfaf0702bce908c68211d1602
kosuke-17 <tamutamu17@icloud.com>
2021-05-16 16:52:23 +0900

    MongoDBに接続

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const productRoute = require("./routes/productRoutes")
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/products', productRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
