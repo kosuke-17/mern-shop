d4ad3892267d132dae81f7ad7b51226a29a46a26
kosuke-17 <tamutamu17@icloud.com>
2021-05-16 15:15:50 +0900

    MongoDBに接続完了

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log("MongoDBに接続成功しました。")
+  } catch (error){
+    console.error("MongoDBに接続失敗しました。")
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
