cad636fe3e3450e4bb4cd6f9a84ff68e32c004a9
kosuke-17 <tamutamu17@icloud.com>
2021-05-16 16:15:05 +0900

    DBにデータを保存

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,11 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
 
     console.log("MongoDBに接続成功しました。")
-  } catch (error){
+  } catch (error) {
     console.error("MongoDBに接続失敗しました。")
     process.exit(1);
   }
