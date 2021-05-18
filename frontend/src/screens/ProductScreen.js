b0b49c9f93a82a5d1489207b0b9e5fb4f7b54c25
kosuke-17 <tamutamu17@icloud.com>
2021-05-18 17:02:03 +0900

    在庫数のデータ表示

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -20,31 +20,34 @@ const ProductScreen = ({ match, history }) => {
   }, [dispatch, product, match]);
 
   return <div className="productscreen">
-      <div className="productscreen__left">
+      {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> : (
+        <>
+         <div className="productscreen__left">
         <div className="left__image">
-          <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="product name" />
+          <img src={product.imageUrl} alt={product.name} />
         </div>
         <div className="left__info">
-          <p className="left__name">商品１</p>
-          <p>価格：20000円</p>
-          <p>商品説明：これは商品１です</p>
+          <p className="left__name">{product.name}</p>
+          <p>価格：${product.price}</p>
+          <p>商品説明：{product.description}</p>
         </div>
       </div>
       <div className="productscreen__right">
         <div className="right__info">
           <p>
-            価格： <span>20000円</span>
+            価格： <span>${product.price}</span>
           </p>
           <p>
-            在庫： <span>あり</span>
+            在庫： <span>{product.countInStock > 0 ? "あり" : "なし"}</span>
           </p>
           <p>
             数量
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
+            <select value={qty} onChange={(e) => setQty(e.target.value)}>
+              {[...Array(product.countInStock).keys()].map((x) => (
+                <option key={x+1} value={x+1}>
+                  {x+1}
+                </option>
+              ))}
             </select>
           </p>
           <p>
@@ -52,6 +55,8 @@ const ProductScreen = ({ match, history }) => {
           </p>
         </div>
       </div>
+        </>
+      )}
     </div>
 };
 
