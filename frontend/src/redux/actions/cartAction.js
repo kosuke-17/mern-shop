8e1aa12843a93cbcd1c528754ae8b593e71ba6d4
kosuke-17 <tamutamu17@icloud.com>
2021-05-18 13:36:57 +0900

    cartの値をreduxで表示

---
+++ frontend/src/redux/actions/cartAction.js
@@ -0,0 +1,20 @@
+import * as actionTypes from '../constants/cartConstants';
+import axios from 'axios';
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/product/${id}`);
+
+  dispatch({
+    type: actionTypes.ADD_TO_CART,
+    payload: {
+      product: data._id,
+      name: data.name,
+      imageUrl: data.imageUrl,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty
+    }
+  })
+
+  localStorage.setItem('cart', JSON.stringify(getState().cart))
+}
